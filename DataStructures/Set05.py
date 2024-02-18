7a5424dbb8c96160e54d9869093e5942304446a3
Suho Kwon <suho.kwon@gmail.com>
2024-02-18 16:33:20 +0900

    Add more examples.

---
+++ DataStructures/Set05.py
@@ -0,0 +1,5 @@
+S = {'red', 'green', 'blue'}
+print(S)
+S.add('yellow')
+print(S)
+# Prints {'blue', 'green', 'yellow', 'red'}
---
+++ DataStructures/Set06.py
@@ -0,0 +1,5 @@
+S = {'red', 'green', 'blue'}
+print(S)
+S.update(['yellow', 'orange'])
+print(S)
+# Prints {'blue', 'orange', 'green', 'yellow', 'red'}
