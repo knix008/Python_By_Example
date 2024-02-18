45ed18152d7f5f624e50979dc952278ac189f32c
Suho Kwon <suho.kwon@gmail.com>
2024-02-18 17:10:24 +0900

    Add more examples.

---
+++ DataStructures/InNotIn01.py
@@ -0,0 +1,11 @@
+# Check for presence
+S = {'red', 'green', 'blue'}
+print(S)
+if 'red' in S:
+    print('yes')
+
+# Check for absence
+S = {'red', 'green', 'blue'}
+print(S)
+if 'yellow' not in S:
+    print('yes')
---
+++ DataStructures/Length01.py
@@ -0,0 +1,4 @@
+S = {'red', 'green', 'blue'}
+print(S)
+print(len(S))
+# Prints 3
