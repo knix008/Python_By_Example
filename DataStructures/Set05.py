7aa1e69e7006f417f8f3cf263236509108d9c6f7
Suho Kwon <suho.kwon@gmail.com>
2024-02-29 17:12:55 +0900

    Add more examples.

--- DataStructures/Set05.py
+++ DataStructures/Set05.py
@@ -2,8 +2,4 @@ S = {'red', 'green', 'blue'}
 print(S)
 S.add('yellow')
 print(S)
-<<<<<<< HEAD
 # Prints {'blue', 'green', 'yellow', 'red'}
-=======
-# Prints {'blue', 'green', 'yellow', 'red'}
->>>>>>> f386ffb (Add more examples.)
