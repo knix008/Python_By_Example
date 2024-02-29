c822bba6d2132a1d9006857f334c96ef87b1fbf3
Suho Kwon <suho.kwon@gmail.com>
2024-02-29 17:10:04 +0900

    Add more examples.

--- DataStructures/Set05.py
+++ DataStructures/Set05.py
@@ -2,4 +2,8 @@ S = {'red', 'green', 'blue'}
 print(S)
 S.add('yellow')
 print(S)
+<<<<<<< HEAD
 # Prints {'blue', 'green', 'yellow', 'red'}
+=======
+# Prints {'blue', 'green', 'yellow', 'red'}
+>>>>>>> f386ffb (Add more examples.)
---
+++ DataStructures/Set08.py
@@ -0,0 +1,9 @@
+S = {'red', 'green', 'blue'}
+print(S)
+x = S.pop()
+print(S)
+# Prints {'green', 'red'}
+
+# removed item
+print("The removed item : ", x)
+# Prints blue
