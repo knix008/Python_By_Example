4517b21d2dbff81fdaacef6b89f9b98fbc20635b
Suho Kwon <suho.kwon@gmail.com>
2024-03-04 12:10:00 +0900

    Add more examples.

---
+++ DataStructures/Set11.py
@@ -0,0 +1,5 @@
+S = {'red', 'green', 'blue'}
+print(S)
+for item in S:
+    print(item)
+# Prints blue green red
---
+++ DataStructures/Set12.py
@@ -0,0 +1,11 @@
+# Check for presence
+S = {'red', 'green', 'blue'}
+print(S)
+if 'red' in S:
+    print('Is red is in the set : yes')
+
+# Check for absence
+S = {'red', 'green', 'blue'}
+print(S)
+if 'yellow' not in S:
+    print('Is yellow not in the set : yes')
