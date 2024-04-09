3cb429ad4bda72e66f43d74358387cbe2c5b36ad
Suho Kwon <suho.kwon@gmail.com>
2024-04-09 13:28:05 +0900

    Add more examples

---
+++ Functions/Functions/Composition01.py
@@ -0,0 +1,5 @@
+import math
+
+x = math.sin(360 * 2 * math.pi)
+print(x)
+# Prints -3.133115067780141e-14
---
+++ Functions/Functions/Composition02.py
@@ -0,0 +1,5 @@
+import math
+
+x = math.exp(math.log(3.14))
+print(x)
+# Prints 3.1399999999999997
